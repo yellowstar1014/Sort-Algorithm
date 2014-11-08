3f9a1f22121aa9a1a2e807325b488ad335403789
Dennis Huang <dennis.xin.huang@gmail.com>
2014-11-07 19:27:53 -0800

    Add insertSortImproved(), insertSortRecursive, binarySearch, binarySearchRecursive

--- Sorter.java
+++ Sorter.java
@@ -2,41 +2,52 @@ import java.util.Random;
 public class Sorter {
   private static int array[];
 
-  public static void main(String args[]) {
-      if (args.length != 0) {
-        array = new int[args.length];
-        for(int i  =0; i < args.length; i++) {
-          int temp = Integer.parseInt(args[i]);
-          array[i] = temp;
-        }
-      }
-      else {
-        array = new int[10];
-        Random random = new Random();
-        for (int i = 0; i < array.length; i++) {
-          array[i] = random.nextInt(10) + 1;
-        }
-      }
-      printArray();
-      //insertSort(array);
-      //insertSortDes(array);
-      //selectSort(array);
-      mergeSort(array, 0, array.length - 1);
-      printArray();
-	}
-
-  public static void printArray() {
-    System.out.println();
-    for(int i = 0;i < array.length; i++) {
-      System.out.print(array[i] + " ");
+  public static void getRandArray() {
+    array = new int[10];
+    Random random = new Random();
+    for (int i = 0; i < array.length; i++) {
+      array[i] = random.nextInt(10) + 1;
     }
   }
 
-  public static void printArray(int[] array) {
-    System.out.println();
+  public static void getArrayFromArgs(String args[]) {
+    array = new int[args.length];
+    for(int i  =0; i < args.length; i++) {
+      int temp = Integer.parseInt(args[i]);
+      array[i] = temp;
+    }
+  }
+
+  public static void main(String args[]) {
+    if (args.length != 0) {
+      getArrayFromArgs(args);
+    }
+    else {
+      getRandArray();
+    }
+    printArray();
+    //insertSort(array);
+    //insertSortDes(array);
+    //insertSortRecursive(array, array.length - 1);
+    insertSortImproved(array);
+    //selectSort(array);
+    //mergeSort(array, 0, array.length - 1);
+    printArray();
+    //printArray();
+	}
+
+  public static void printArray() {
     for(int i = 0;i < array.length; i++) {
       System.out.print(array[i] + " ");
     }
+    System.out.println();
+  }
+
+  public static void printArray(int[] array) {
+    for(int i = 0;i < array.length; i++) {
+      System.out.print(array[i] + " ");
+    }
+    System.out.println();
   }
 
   /* loop invariant
@@ -58,6 +69,34 @@ public class Sorter {
         j--;
       }
       array[j+1] = key;
+    }
+  }
+
+  public static void insertSortImproved(int[] array) {
+    for (int i = 1; i < array.length; i++) {
+      int key = array[i];
+      int endIndex = i - 1;
+      int insertIndex = binarySearchVariant(key, array, endIndex);
+      for(int j = endIndex; j >= insertIndex; j--) {
+        array[j+1] = array[j];
+      }
+      array[insertIndex] = key;
+    }
+  }
+  /*
+  loop invariant
+  1. array[0, endIndex) is sorted
+  */
+  public static void insertSortRecursive(int[] array, int endIndex) {
+    if(endIndex != 0) {
+      insertSortRecursive(array, endIndex - 1);
+      int j = endIndex - 1;
+      int key = array[endIndex];
+      while (j >= 0 && array[j] > key) {
+        array[j + 1] = array[j];
+        j--;
+      }
+      array[j + 1] = key;
     }
   }
 
@@ -120,6 +159,106 @@ public class Sorter {
       swap(array, findMin(array, numbersorted), numbersorted);
     }
   }
+  /*
+    loop invariant:
+    arraySorted[0,startIndex) < key
+    arraySorted(endIndex,(arraySorted.length - 1)] > key
+  */
+  public static int binarySearch(int key, int[] arraySorted) {
+    int startIndex = 0;
+    int endIndex = arraySorted.length - 1;
+    while (startIndex <= endIndex) {
+      int splitIndex = (startIndex + endIndex) / 2;
+      if(key > arraySorted[splitIndex]) {
+        startIndex = splitIndex + 1;
+      }
+      else if(key < arraySorted[splitIndex]) {
+        endIndex = splitIndex - 1;
+      }
+      else {
+        return splitIndex;
+      }
+    }
+    return -1;
+  }
+
+  public static int binarySearchRecursive(int key, int[] arraySorted, int startIndex, int endIndex) {
+    if(startIndex <= endIndex) {
+      int splitIndex = (startIndex + endIndex) / 2;
+      if (key == arraySorted[splitIndex]) {
+        return splitIndex;
+      }
+      else if (key > arraySorted[splitIndex]) {
+        return binarySearchRecursive(key, arraySorted, splitIndex + 1, endIndex);
+      }
+      else {
+        return binarySearchRecursive(key, arraySorted, startIndex, splitIndex - 1);
+      }
+    }
+    else {
+      return -1;
+    }
+  }
+
+  /*
+  return the Index x where array[x] == key or array[x-1] is the
+  first element who is smaller than key from back to front.
+  put it simpe, return the index x where key should be inserted in.
+  */
+
+  public static int binarySearchVariantRecur(int key, int[] arraySorted, int left, int right, int lastMid) {
+    if (left > right) {
+      if(key > arraySorted[lastMid]) {
+        return lastMid + 1;
+      }
+      else
+      {
+        return lastMid;
+      }
+    }
+    else{
+      int mid = (left + right) / 2;
+      if (key == arraySorted[mid]) {
+        return mid;
+      }
+      else if (key > arraySorted[mid]) {
+        return binarySearchVariantRecur(key, arraySorted, mid + 1, right, mid);
+      }
+      else {
+        return binarySearchVariantRecur(key, arraySorted, left, mid - 1, mid);
+      }
+    }
+  }
+
+  /*
+  return the Index x where array[x] == key or array[x-1] is the
+  first element who is smaller than key from back to front.
+  put it simpe, return the index x where key should be inserted in.
+  */
+
+  public static int binarySearchVariant(int key, int[] arraySorted, int endIndex) {
+    int left = 0;
+    int right = endIndex;
+    int mid = (right + left) / 2;
+    while(left <= right) {
+      mid = (right + left) / 2;
+      if (key == arraySorted[mid]) {
+        break;
+      }
+      else if (key > arraySorted[mid]) {
+        left = mid + 1;
+      }
+      else {
+        right = mid - 1;
+      }
+    }
+    if (key <= arraySorted[mid]) {
+      return mid;
+    }
+    else {
+      return mid + 1;
+    }
+  }
 
   public static void mergeSort(int[] array, int startIndex, int endIndex) {
     if (startIndex < endIndex) {
@@ -133,51 +272,6 @@ public class Sorter {
     }
   }
 
-  // merge without etra setinel element
-  public static void merge2(int[] array, int startIndex, int splitIndex, int endIndex) {
-    int L_array[] = new int[splitIndex - startIndex + 1];
-    int R_array[] = new int[endIndex - splitIndex];
-
-    for (int i = 0; i < splitIndex - startIndex + 1; i++) {
-      L_array[i] = array[startIndex + i];
-    }
-    for (int i = 0; i < endIndex - splitIndex; i++) {
-      R_array[i] = array[splitIndex + 1 + i];
-    }
-
-    int L_Index = 0;
-    int R_Index = 0;
-    int A_Index = startIndex;
-
-    while (L_Index <= (splitIndex - startIndex) && R_Index <= (endIndex  - splitIndex - 1)) {
-      if (L_array[L_Index] > R_array[R_Index]) {
-        array[A_Index] = R_array[R_Index];
-        R_Index++;
-        A_Index++;
-      }
-      else {
-        array[A_Index] = L_array[L_Index];
-        L_Index++;
-        A_Index++;
-      }
-    }
-
-    if (R_Index <= (endIndex  - splitIndex - 1)) {
-      while (A_Index <= endIndex) {
-        array[A_Index] = R_array[R_Index];
-        A_Index++;
-        R_Index++;
-      }
-    }
-
-    if (L_Index <= (splitIndex - startIndex)) {
-      while (A_Index <= endIndex) {
-        array[A_Index] = L_array[L_Index];
-        A_Index++;
-        L_Index++;
-      }
-    }
-  }
   /*
   loop invariant
   1. array[startIndex, startIndex + numberMerged) are the smallest numberMerged
@@ -229,6 +323,52 @@ public class Sorter {
     }
 
     if (L_array[L_Index] != MAX_NUM) {
+      while (A_Index <= endIndex) {
+        array[A_Index] = L_array[L_Index];
+        A_Index++;
+        L_Index++;
+      }
+    }
+  }
+
+  // merge without etra setinel element
+  public static void merge2(int[] array, int startIndex, int splitIndex, int endIndex) {
+    int L_array[] = new int[splitIndex - startIndex + 1];
+    int R_array[] = new int[endIndex - splitIndex];
+
+    for (int i = 0; i < splitIndex - startIndex + 1; i++) {
+      L_array[i] = array[startIndex + i];
+    }
+    for (int i = 0; i < endIndex - splitIndex; i++) {
+      R_array[i] = array[splitIndex + 1 + i];
+    }
+
+    int L_Index = 0;
+    int R_Index = 0;
+    int A_Index = startIndex;
+
+    while (L_Index <= (splitIndex - startIndex) && R_Index <= (endIndex  - splitIndex - 1)) {
+      if (L_array[L_Index] > R_array[R_Index]) {
+        array[A_Index] = R_array[R_Index];
+        R_Index++;
+        A_Index++;
+      }
+      else {
+        array[A_Index] = L_array[L_Index];
+        L_Index++;
+        A_Index++;
+      }
+    }
+
+    if (R_Index <= (endIndex  - splitIndex - 1)) {
+      while (A_Index <= endIndex) {
+        array[A_Index] = R_array[R_Index];
+        A_Index++;
+        R_Index++;
+      }
+    }
+
+    if (L_Index <= (splitIndex - startIndex)) {
       while (A_Index <= endIndex) {
         array[A_Index] = L_array[L_Index];
         A_Index++;
